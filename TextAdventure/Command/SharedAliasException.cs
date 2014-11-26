9293e28e409fb75e0f7915c1da851a3d153712e2
Devin Price <devin@nowyoujump.com>
2014-11-26 14:13:24 -0500

    Add SharedAliasException.cs

---
+++ TextAdventure/Command/SharedAliasException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextAdventure.Command
+{
+    public class SharedAliasException : Exception
+    {
+        public string Alias { get; private set; }
+
+        public SharedAliasException(string alias)
+            : base("The specified alias references more than one command.")
+        {
+            Alias = alias;
+        }
+    }
+}
