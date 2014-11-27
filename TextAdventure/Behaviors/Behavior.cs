0eff389b7bebbf3a0d39b633d6292aad23365589
Devin Price <devin@nowyoujump.com>
2014-11-26 23:44:31 -0500

    Stubbed out behaviors

---
+++ TextAdventure/Behaviors/Behavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Behaviors
+{
+    public class Behavior : IBehavior
+    {
+        public int Priority { get; set; }
+        public bool Active { get; protected set; }
+        public int Mask { get; set; }
+
+        public bool ShouldUpdate
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public virtual void Start()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Stop()
+        {
+
+        }
+    }
+}
---
+++ TextAdventure/Behaviors/IBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Behaviors
+{
+    public interface IBehavior
+    {
+        int Priority { get; }
+        bool Active { get; }
+        int Mask { get; }
+        bool ShouldUpdate { get; }
+
+        void Start();
+        void Update();
+        void Stop();
+    }
+}
