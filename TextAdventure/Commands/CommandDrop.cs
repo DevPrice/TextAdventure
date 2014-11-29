5f4395de82e695a29d2d20bc247dcf72b600415b
Devin Price <price.devin.gm@gmail.com>
2014-11-28 19:36:16 -0500

    Added CommandDrop

---
+++ TextAdventure/Commands/CommandDrop.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventure.Entities;
+using TextAdventure.Items;
+using TextAdventure.Utility;
+using TextAdventure.World;
+
+namespace TextAdventure.Commands
+{
+    public class CommandDrop : Command<Item>
+    {
+        public CommandDrop(GameWorld world, ICommandSender sender, Item item)
+            : base(world, sender, item)
+        {
+            Name = "drop";
+            Description = "Drops an item you are carrying.";
+            Usage = "drop [item]";
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            if (Target != null)
+            {
+                IMapNode currentNode = World.Map.LocationOf(((Player)Sender));
+
+                ((Player)Sender).Inventory.Remove(Target);
+                currentNode.Items.Add(Target);
+
+                Output.WriteLine("You drop the {0}.", Target.Name);
+            }
+            else
+            {
+                Output.WriteLine("You can't drop that.");
+            }
+        }
+
+        public override ICommand Create(ICommandSender sender, string[] args)
+        {
+            if (sender is Player && args.Length >= 2)
+            {
+                string[] itemNameArr = new string[args.Length - 1];
+                Array.Copy(args, 1, itemNameArr, 0, itemNameArr.Length);
+                string itemName = String.Join(" ", itemNameArr);
+
+                Item droppedItem = null;
+
+                foreach (Item item in ((Player)sender).Inventory)
+                {
+                    if (item.Name.Equals(itemName, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        droppedItem = item;
+                        break;
+                    }
+                }
+
+                return new CommandDrop(World, sender, droppedItem);
+            }
+
+            return new CommandDrop(World, sender, null);
+        }
+    }
+}
