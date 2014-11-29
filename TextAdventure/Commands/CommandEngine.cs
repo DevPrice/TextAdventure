f889f4b43a24d911fcd65cdaf2e76ee7a75c3527
Devin Price <price.devin.gm@gmail.com>
2014-11-28 22:19:42 -0500

    Added CommandEngine.cs

---
+++ TextAdventure/Commands/CommandEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Commands
+{
+    public class CommandEngine
+    {
+        public CommandParser Parser { get; private set; }
+
+        public CommandEngine(CommandParser parser)
+        {
+            Parser = parser;
+        }
+
+        public void RunCommand(string command, ICommandSender sender)
+        {
+            try
+            {
+                ICommand commandObj = Parser.Parse(command, sender);
+
+                try
+                {
+                    commandObj.Execute();
+                }
+                catch (UsageException)
+                {
+                    sender.SendMessage("Invalid usage.");
+                }
+                catch (InsufficientPermissionException)
+                {
+                    sender.SendMessage("Invalid permissions.");
+                }
+            }
+            catch (CommandNotFoundException)
+            {
+                sender.SendMessage("Invalid command.");
+            }
+        }
+    }
+}
