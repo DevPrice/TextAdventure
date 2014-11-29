56ade56b5d437e5fbde95f86b1a1e70737cac9df
Devin Price <price.devin.gm@gmail.com>
2014-11-28 21:30:05 -0500

    Added files missing from last commit

---
+++ TextAdventure/Entities/RemotePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Entities
+{
+    public class RemotePlayer : Player
+    {
+        public IPEndPoint EndPoint { get; private set; }
+
+        public RemotePlayer(IPEndPoint endPoint)
+        {
+            EndPoint = endPoint;
+        }
+
+        public override void SendMessage(string message)
+        {
+
+        }
+    }
+}
---
+++ TextAdventureClient/TextAdventureClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureClient
+{
+    class TextAdventureClient
+    {
+        const int DEFAULT_PORT = 53251;
+        static UdpClient Client;
+
+        static void Main(string[] args)
+        {
+            Client = new UdpClient(0);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, DEFAULT_PORT);
+
+            Task listenTask = new Task(Listen);
+            listenTask.Start();
+            
+            while (true)
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(Console.ReadLine());
+                Client.Send(bytes, bytes.Length, serverEndPoint);
+            }
+        }
+
+        private static void Listen()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, DEFAULT_PORT);
+
+            while (true)
+            {
+                byte[] bytes = Client.Receive(ref endPoint);
+
+                string message = Encoding.Unicode.GetString(bytes);
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
